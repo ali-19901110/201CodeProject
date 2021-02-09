b4aaa185401179ffa1c6f8f33a4eef834e8c9173
ali <alikasasbeh60@yahoo.com>
2021-02-09 19:07:09 +0200

    add new Q

--- js/app.js
+++ js/app.js
@@ -1,65 +1,136 @@
 'use strict';
 // alert('hellllllllllo');
-
-let r0 =prompt ('Do you prefer your coffee black?');
+ let yourGrad =0;
+let r0 =prompt ('Do i prefer coffee black?');
 console.log(r0);
 if(r0.toLowerCase()==='yes'||r0.toLowerCase()=='y'){
     alert("You'r same me");
+    yourGrad++;
 }else if(r0.toLowerCase()==='no'||r0.toLowerCase()==='y')
 {
-  alert("you'r don't same me");
+  alert('you are do not same me');
 }else{
    
     alert('you should answer yes/no');
 }
  
- let r1=prompt ('Do you prefer your color black?');
+ let r1=prompt ('Do you i prefer  color black?');
  console.log(r1);
 if(r1.toLowerCase()==='yes'||r1.toLowerCase()=='y'){
     alert("You'r same me");
+    yourGrad++;
 }else if(r1.toLowerCase()==='no'||r1.toLowerCase()==='y')
 {
-    alert('you should answer yes/no');
+    alert('you are do not same me');
 }else{
     
-    alert('Nice too meet you');
+    alert('you should answer yes/no');
 }
  let r2 =prompt ('Do you prefer your  footbal?');
  console.log(r2);
 if(r2.toLowerCase()==='yes'||r2.toLowerCase()=='y'){
     alert("You'r same me");
+    yourGrad++;
 }else if(r2.toLowerCase()==='no'||r2.toLowerCase()==='y')
 {
-    alert('you should answer yes/no');
+    alert('you are do not same me');
 }else{
    
-    alert('Nice too meet you'+ userNam);
+    alert('you should answer yes/no');
 }
- let r3 =prompt ('Do you prefer your games?');
+ let r3 =prompt ('Do i prefer  games?');
 console.log(r3);
 if(r3.toLowerCase()==='yes'||r3.toLowerCase()=='y'){
     alert("You'r same me");
+    yourGrad++;
 }else if(r3.toLowerCase()==='no'||r3.toLowerCase()==='y')
 {
-    alert('you should answer yes/no');
+    alert('you are do not same me');
 }else{
    
     alert('you should answer yes/no');
 }
- let r4 =prompt ('Do you prefer your markitng?');
+ let r4 =prompt ('Do i prefer  markitng?');
 console.log(r4);
 if(r4.toLowerCase()==='yes'||r4.toLowerCase()=='y'){
     alert("You'r same me");
+    yourGrad++;
 }else if(r4.toLowerCase()==='no'||r4.toLowerCase()==='y')
 {
-  alert("you'r don't same me");
+  alert('you are do not same me');
 }else{
    
     alert('you should answer yes/no');
 }
 
 let userNam =prompt ('Please enter your name?');
-alert("?You'r welcom to my website");
+alert(userNam +' welcom to my website');
+
+
+for(let i=0;i<4;i++){
+    
+let userGues6 =prompt('what is my age?');
+
+ if(parseInt(userGues6) ===30){
+    alert('succeed');
+    yourGrad++
+    break;
+}else if(parseInt(userGues6)>30){
+    alert('too high')
+}
+else if(parseInt(userGues6)<30){
+    alert('too low')  
+}
+
+alert('you will get another try');
+}
+
+let arr =['red' ,'black','pink','green'];
+
+
+let x=yourGrad+1;
+let y=x;
+// alert(x);
+for(let i=0;i<6;i++){
+    //alert(i);
+let userGues7 =prompt('what is my favorite color?');
+
+switch(userGues7.toString()){
+    
+    case 'red' :
+                alert('win');
+                yourGrad++
+                break;
+    case 'black':
+               alert('win');
+               yourGrad++
+               break;
+    case 'pink':
+                alert('win');
+                yourGrad++
+                break;
+    case 'green':
+                alert('win');
+                yourGrad++
+                break;
+                default:
+                    alert ('incorrect answer');
+  }
+      if(y === yourGrad){
+          break;
+      }
+}
+for(let i=0;i<4;i++){
+
+    alert('this is my answer :  '+arr[i]);
+}
+
+alert('you are grad is  :'+yourGrad );
+
+
+
+
+
 // console.log(userInter1);
 // console.log(userInter2);
 // console.log(userInter3);
