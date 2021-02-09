de1053e4dd786b6016d9bd2c6b7ab5e1826bae68
ali <alikasasbeh60@yahoo.com>
2021-02-09 14:43:34 +0200

    change my code js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,69 @@
 'use strict';
 // alert('hellllllllllo');
-let userInter0 =prompt ('Do you prefer your coffee black?');
-let userInter1=prompt ('Do you prefer your color black?');
-let userInter2 =prompt ('Do you prefer your  footbal?');
-let userInter3 =prompt ('Do you prefer your games?');
-let userInter4 =prompt ('Do you prefer your markitng?');
-console.log(userInter0);
-console.log(userInter1);
-console.log(userInter2);
-console.log(userInter3);
-console.log(userInter4);
+
+let r0 =prompt ('Do you prefer your coffee black?');
+console.log(r0);
+if(r0.toLowerCase()==='yes'||r0.toLowerCase()=='y'){
+    alert("You'r same me");
+}else if(r0.toLowerCase()==='no'||r0.toLowerCase()==='y')
+{
+  alert("you'r don't same me");
+}else{
+   
+    alert('you should answer yes/no');
+}
+ 
+ let r1=prompt ('Do you prefer your color black?');
+ console.log(r1);
+if(r1.toLowerCase()==='yes'||r1.toLowerCase()=='y'){
+    alert("You'r same me");
+}else if(r1.toLowerCase()==='no'||r1.toLowerCase()==='y')
+{
+    alert('you should answer yes/no');
+}else{
+    
+    alert('Nice too meet you');
+}
+ let r2 =prompt ('Do you prefer your  footbal?');
+ console.log(r2);
+if(r2.toLowerCase()==='yes'||r2.toLowerCase()=='y'){
+    alert("You'r same me");
+}else if(r2.toLowerCase()==='no'||r2.toLowerCase()==='y')
+{
+    alert('you should answer yes/no');
+}else{
+   
+    alert('Nice too meet you'+ userNam);
+}
+ let r3 =prompt ('Do you prefer your games?');
+console.log(r3);
+if(r3.toLowerCase()==='yes'||r3.toLowerCase()=='y'){
+    alert("You'r same me");
+}else if(r3.toLowerCase()==='no'||r3.toLowerCase()==='y')
+{
+    alert('you should answer yes/no');
+}else{
+   
+    alert('you should answer yes/no');
+}
+ let r4 =prompt ('Do you prefer your markitng?');
+console.log(r4);
+if(r4.toLowerCase()==='yes'||r4.toLowerCase()=='y'){
+    alert("You'r same me");
+}else if(r4.toLowerCase()==='no'||r4.toLowerCase()==='y')
+{
+  alert("you'r don't same me");
+}else{
+   
+    alert('you should answer yes/no');
+}
+
+let userNam =prompt ('Please enter your name?');
+alert("?You'r welcom to my website");
+// console.log(userInter1);
+// console.log(userInter2);
+// console.log(userInter3);
+// console.log(userInter4);
 // alert(userInter0);
 // if(userInter0==='yes')
 // {alert(userInter0);
@@ -18,12 +72,49 @@ console.log(userInter4);
 // alert(userInter0.toUpperCase());
 
 
-if((userInter0.toLowerCase() ==='yes')||(userInter0.toUpperCase() ==='yes') && (userInter1.toLowerCase() ==='yes')||(userInter1.toUpperCase() ==='yes')
-&&(userInter2.toLowerCase() ==='yes')||(userInter2.toUpperCase() ==='yes')
-&&(userInter3.toLowerCase() ==='yes')||(userInter3.toUpperCase() ==='yes')
-&&(userInter4.toLowerCase() ==='yes')||(userInter4.toUpperCase() ==='yes'))
+// if((userInter0.toLowerCase() ==='yes')||(userInter0.toUpperCase() ==='yes') && (userInter1.toLowerCase() ==='yes')||(userInter1.toUpperCase() ==='yes')
+// &&(userInter2.toLowerCase() ==='yes')||(userInter2.toUpperCase() ==='yes')
+// &&(userInter3.toLowerCase() ==='yes')||(userInter3.toUpperCase() ==='yes')
+// &&(userInter4.toLowerCase() ==='yes')||(userInter4.toUpperCase() ==='yes'))
      
-     {
-        let userInter5 =prompt ('Please enter your name?');
-         alert(userInter5 + "welcom to my website ");
-     }
+//      {
+//         let userInter5 =prompt ('Please enter your name?');
+//          alert(userInter5 + "welcom to my website ");
+//      }
+
+
+// let answer = prompt('do you think i like lodon');
+// switch(answer.toLocaleLowerCase()){
+//     case 'yes':
+//     case 'y'  :
+//         alert('hats incorrect');
+//         break;
+//     case 'no' :
+//     case  'n' :
+//         alert('thats correct');
+//         break;
+//         case 'idk':
+//       alert('ali');   
+//       break;
+//       default:
+//           alert('italy') ; 
+// }
+
+
+
+
+
+
+// let arr =[1,2,3,4,5,6,7,8,9,10];
+// //alert(arr.length);
+// // arr.push
+// for(var i=0;i<=arr.length;i++)
+// {
+//     // console.log(arr[i]);
+//     for(let j=1;j<=arr.length;j++){
+//        let x= (arr[i])*j;
+//        document.write(x+'&nbsp&nbsp');
+
+//  }
+//     document.write('<br>'+'<br>');
+// }
